ea73e99d9c1983d81a1c349dff0bbc7cb1cdd5bb
KuzkinaOlga <o.kuzkina@gmail.com>
2022-08-14 14:43:09 +0200

    fixed Service

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getMovieReviews } from 'service/api-service';
+import { getMovieReviews } from 'Service/api-service';
 
 export const Reviews = () => {
   const { movieId } = useParams();
